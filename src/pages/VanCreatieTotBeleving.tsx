6671d4cad98687fa8c50ab410edcf989e1aabc9b
innovisuals <innovisuals7@gmail.com>
2025-03-29 01:00:23 +0100

    Update VanCreatieTotBeleving.tsx

--- src/pages/VanCreatieTotBeleving.tsx
+++ src/pages/VanCreatieTotBeleving.tsx
@@ -20,7 +20,6 @@ const VanCreatieTotBeleving: React.FC = () => {
             <h2 className="text-3xl md:text-4xl font-bold mb-12 animate-fade-in text-left" style={{ color: "#5c899d" }}>
   Van Creatie tot Beleving
 </h2>
-
             
             <div className="space-y-8 mt-4">
               <div className="animate-fade-in" style={{ animationDelay: "100ms" }}>
